2ca7a7bbef2b5e3b0f2a6cce8762d189fb6e7975
Cezary Zysk <cezaryy.z97@gmail.com>
2021-06-23 00:55:12 +0200

    Added shopping cart view

---
+++ eMovieStore/Controllers/ShoppingCartController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eMovieStore.Controllers
+{
+    public class ShoppingCartController : Controller
+    {
+        public ShoppingCartController()
+        {
+
+
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
